86b9382a9d569b591a6cb93828337e6d1724435b
Brittney <bje3dm@mail.umck.edu>
2020-02-09 21:23:43 -0600

    Updated app for Version 2.0

--- app/src/main/java/com/example/umpirebuddy10/MainActivity.java
+++ app/src/main/java/com/example/umpirebuddy10/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.umpirebuddy10;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Button reset  = findViewById(R.id.reset);
         Button sInc   = findViewById(R.id.sInc);
         Button bInc   = findViewById(R.id.bInc);
+        Button about  = findViewById(R.id.about);
 
         strikeCount = findViewById(R.id.strikeCount);
         ballCount = findViewById(R.id.ballCount);
@@ -30,36 +34,102 @@ public class MainActivity extends AppCompatActivity {
         sInc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                strikeNum++;
-                strikeCount.setText(Integer.toString(strikeNum));
+
+                if(strikeNum < 3 && ballNum < 4)
+                {
+                    strikeNum++;
+                    strikeCount.setText(Integer.toString(strikeNum));
+                }
+
+                if(strikeNum == 3)
+                {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Three Strikes!")
+                            .setMessage("Batter's Out!")
+                            .setNeutralButton("Close", null)
+                            .setIcon(android.R.drawable.stat_sys_warning).show();
+                }
+
+
             }
         });
 
         bInc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ballNum++;
-                ballCount.setText(Integer.toString(ballNum));
+
+                if(ballNum < 4 && strikeNum < 3)
+                {
+                    ballNum++;
+                    ballCount.setText(Integer.toString(ballNum));
+                }
+
+                if(ballNum == 4)
+                {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Four Balls!")
+                            .setMessage("Batter Walks!")
+                            .setNeutralButton("Close", null)
+                            .setIcon(android.R.drawable.stat_sys_warning).show();
+                }
             }
         });
 
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-                System.exit(0);
+
+                if(strikeNum < 3 && ballNum < 4)
+                {
+                    finish();
+                    System.exit(0);
+                }
+
+                if(strikeNum == 3)
+                {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Batter is out!")
+                            .setMessage("Reset the counts.")
+                            .setNeutralButton("Close", null)
+                            .setIcon(android.R.drawable.stat_sys_warning).show();
+                }
+
+                if(ballNum == 4)
+                {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Batter has walked!")
+                            .setMessage("Reset the counts.")
+                            .setNeutralButton("Close", null)
+                            .setIcon(android.R.drawable.stat_sys_warning).show();
+                }
+
             }
         });
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;               strikeNum = 0;
+                strikeNum = 0;
                 ballNum = 0;
 
                 strikeCount.setText(Integer.toString(strikeNum));
                 ballCount.setText(Integer.toString(ballNum));
             }
         });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(strikeNum < 3 && ballNum < 4) {
+                    openAbout();
+                }
+            }
+        });
+    }
+
+    public void openAbout() {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
     }
 }
